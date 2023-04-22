0474620c4acc77b73ecb819a627a6156c51fc5c2
mohamed99elsokary <mohamed99elsokary@gmail.com>
2023-04-22 19:22:15 +0200

    add product serializer

--- pixbum/productapp/serializers.py
+++ pixbum/productapp/serializers.py
@@ -1,2 +1,9 @@
 from rest_framework import serializers
+
 from . import models
+
+
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.Product
+        fields = "__all__"

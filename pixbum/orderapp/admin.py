4e1ff423d930b681d1c317709194671b0638d501
mohamed99elsokary <mohamed99elsokary@gmail.com>
2023-04-22 20:23:24 +0200

    add order and order details models

--- pixbum/orderapp/admin.py
+++ pixbum/orderapp/admin.py
@@ -1,2 +1,14 @@
 from django.contrib import admin
+from unfold.admin import ModelAdmin
+
 from . import models
+
+
+@admin.register(models.Order)
+class OrderAdmin(ModelAdmin):
+    """Admin View for Order"""
+
+
+@admin.register(models.OrderDetails)
+class OrderDetailsAdmin(ModelAdmin):
+    """Admin View for OrderDetails"""
--- pixbum/orderapp/models.py
+++ pixbum/orderapp/models.py
@@ -1,1 +1,27 @@
 from django.db import models
+
+from pixbum.productapp.models import Product
+from pixbum.servicesapp.models import Service
+from pixbum.userapp.models import Address, User
+
+
+class Order(models.Model):
+    # relations
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    address = models.ForeignKey(Address, on_delete=models.CASCADE)
+    # fields
+
+    def __str__(self):
+        return self.name
+
+
+class OrderDetails(models.Model):
+    # relations
+    oder = models.ForeignKey(Order, on_delete=models.CASCADE)
+    service = models.ForeignKey(Service, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    # fields
+    pdf = models.FileField(upload_to=None, max_length=100)
+
+    def __str__(self):
+        return self.name

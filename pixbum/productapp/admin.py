cc0a20187f6b56ccfd7fecbff60da108760a1b89
mohamed99elsokary <mohamed99elsokary@gmail.com>
2023-04-22 19:13:31 +0200

    add product app models to admin

--- pixbum/productapp/admin.py
+++ pixbum/productapp/admin.py
@@ -1,2 +1,19 @@
 from django.contrib import admin
+from unfold.admin import ModelAdmin
+
 from . import models
+
+
+@admin.register(models.Product)
+class ProductAdmin(ModelAdmin):
+    """Admin View for Product"""
+
+
+@admin.register(models.ProductFeatures)
+class ProductFeaturesAdmin(ModelAdmin):
+    """Admin View for ProductFeatures"""
+
+
+@admin.register(models.ProductImages)
+class ProductImagesAdmin(ModelAdmin):
+    """Admin View for ProductImages"""
--- pixbum/servicesapp/admin.py
+++ pixbum/servicesapp/admin.py
@@ -1,13 +1,14 @@
 from django.contrib import admin
+from unfold.admin import ModelAdmin
 
 from . import models
 
 
 @admin.register(models.Service)
-class ServiceAdmin(admin.ModelAdmin):
+class ServiceAdmin(ModelAdmin):
     """Admin View for Service"""
 
 
 @admin.register(models.Category)
-class ServiceAdmin(admin.ModelAdmin):
+class ServiceAdmin(ModelAdmin):
     """Admin View for Service"""

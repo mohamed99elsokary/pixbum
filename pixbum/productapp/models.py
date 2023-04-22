624d2e226ac2589a157c062a1b4687658d7cd486
mohamed99elsokary <mohamed99elsokary@gmail.com>
2023-04-22 19:06:13 +0200

    add Product,ProductImages,ProductFeatures models

--- pixbum/productapp/models.py
+++ pixbum/productapp/models.py
@@ -1,1 +1,46 @@
 from django.db import models
+
+from pixbum.servicesapp.models import Category, Service
+
+
+class Product(models.Model):
+    # relations
+    service = models.ForeignKey(
+        Service, on_delete=models.CASCADE, related_name="service_products"
+    )
+    category = models.ForeignKey(
+        Category, on_delete=models.CASCADE, related_name="category_products"
+    )
+    # fields
+    size = models.CharField(max_length=50)
+    name = models.CharField(max_length=50)
+    min_photos_amount = models.IntegerField(default=1)
+    max_photos_amount = models.IntegerField(default=1)
+
+    def __str__(self):
+        return self.name
+
+
+class ProductImages(models.Model):
+    # relations
+    product = models.ForeignKey(
+        Product, on_delete=models.CASCADE, related_name="product_images"
+    )
+    # fields
+    image = models.ImageField(
+        upload_to="media/", height_field=None, width_field=None, max_length=None
+    )
+
+
+class ProductFeatures(models.Model):
+    product = models.ForeignKey(
+        Product, on_delete=models.CASCADE, related_name="product_features"
+    )
+    # fields
+    logo = models.ImageField(
+        upload_to="media/", height_field=None, width_field=None, max_length=None
+    )
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name

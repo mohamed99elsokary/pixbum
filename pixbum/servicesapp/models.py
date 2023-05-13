ed063e56981331525b0f5c5c27b929a1609890f7
mohamed99elsokary <mohamed99elsokary@gmail.com>
2023-05-14 01:06:41 +0300

    add image to service

--- pixbum/servicesapp/models.py
+++ pixbum/servicesapp/models.py
@@ -6,6 +6,7 @@ class Service(models.Model):
 
     # fields
     name = models.CharField(max_length=50)
+    image = models.ImageField(upload_to="media/", default=None, null=True, blank=True)
 
     def __str__(self):
         return self.name

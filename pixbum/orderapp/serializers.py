9d9e9fc26660d3f2dab7e69685c80463055f90df
mohamed99elsokary <mohamed99elsokary@gmail.com>
2023-05-28 01:33:15 +0300

    fix get user order class

--- pixbum/orderapp/serializers.py
+++ pixbum/orderapp/serializers.py
@@ -23,7 +23,7 @@ class DefaultUserOrder:
 
     def __call__(self, serializer_field):
         user = serializer_field.context["request"].user
-        order = models.Order.objects.filter(user=user)
+        order = models.Order.objects.filter(user=user, is_checkout=False)
         if order:
             return order.first()
         return models.Order.objects.create(
@@ -35,37 +35,22 @@ class DefaultUserOrder:
 
 
 class OrderDetailsSerializer(serializers.ModelSerializer):
-    order = serializers.SlugRelatedField(read_only=True, slug_field="id")
+    order = serializers.HiddenField(default=DefaultUserOrder())
 
     class Meta:
         model = models.OrderDetails
+        read_only_fields = ("price",)
         exclude = ("pdf",)
-
-    def create(self, validated_data):
-        user = self.context["request"].user
-        user_address = models.Address.objects.filter(user=user).first()
-        validated_data["order"] = models.Order.objects.create(
-            user=user, address=user_address
-        )
-        return super().create(validated_data)
 
 
 class OrderSerializer(serializers.ModelSerializer):
     order_details = OrderDetailsSerializer(many=True)
     user = serializers.HiddenField(default=serializers.CurrentUserDefault())
-    total = serializers.SerializerMethodField()
     address = AddressSerializer()
 
     class Meta:
         model = models.Order
         fields = "__all__"
-
-    def get_total(self, obj):
-        total = 0
-        for product in obj.order_details.all():
-            price = product.product.price * product.quantity
-            total += price
-        return total
 
 
 class DraftSerializer(serializers.ModelSerializer):

9b04f0857610a9f88feccc46b80c949b6e129c82
mohamed99elsokary <mohamed99elsokary@gmail.com>
2023-06-25 01:30:18 +0300

    fix bulk notifications admin

--- pixbum/notificationapp/admin.py
+++ pixbum/notificationapp/admin.py
@@ -1,11 +1,19 @@
 from distutils.command import register
 
+from bit68_notifications.models import BulkNotification
 from django.contrib import admin
 from unfold.admin import ModelAdmin
 
 from . import models
 
+admin.site.unregister(BulkNotification)
+
 
 @admin.register(models.Notification)
 class NotificationAdmin(ModelAdmin):
     """Admin View for Notification"""
+
+
+@admin.register(BulkNotification)
+class BulkNotificationAdmin(ModelAdmin):
+    """Admin View for BulkNotification"""
